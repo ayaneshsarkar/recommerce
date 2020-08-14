7000b99974f89cd46287ba214de46a27a0606884
Ayanesh Sarkar <ayaneshofficial@gmail.com>
2020-08-14 23:49:39 +0530

    Initial Commit :heavy_check_mark:

--- src/components/admin/AddBook.js
+++ src/components/admin/AddBook.js
@@ -4,10 +4,13 @@ class AddBook extends Component {
   render() {
     return (
       <div className="container">
-        AddBook
+        <div className="col-12">
+          <h3 className="form_title mb-5">Add Books</h3>
+        </div>
       </div>
     );
   }
 }
+
 
 export default AddBook;
